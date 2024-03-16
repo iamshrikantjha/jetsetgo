b150ce11da0a6864e01afd605819b5cc862746ae
Shrikant Jha <shrikantjha11@gmail.com>
2024-03-17 00:58:21 +0530

    custom fonts added

---
+++ src/screens/ProfileScreen/ProfileScreenComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
+const ProfileScreenComponent = () => {
+  return (
+    <View>
+      <Text>ProfileScreenComponent</Text>
+    </View>
+  )
+}
+
+export default ProfileScreenComponent
