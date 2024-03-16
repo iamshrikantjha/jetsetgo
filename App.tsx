21866a85e2b6721fae93a899734fcdcba5dfeb16
Shrikant Jha <shrikantjha11@gmail.com>
2024-03-17 01:21:48 +0530

    vector icons added + working

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import Entypo from "react-native-vector-icons/Entypo";
 
 const App = () => {
   return (
     <NavigationContainer>
       <View>
-        <Text>App</Text>
+        <Text style={{
+          fontFamily: 'Lato-Black',
+          fontSize: 32,
+        }}>App</Text>
       </View>
+      <Entypo name="500px" size={30} color="teal" />
+
     </NavigationContainer>
   );
 };
