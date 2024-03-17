48b03722c61c5a4aa63d18ead35b3864915a4e32
Shrikant Jha <shrikantjha11@gmail.com>
2024-03-17 13:27:22 +0530

    Update TripsScreenComponent.tsx

--- src/screens/TripsScreen/TripsScreenComponent.tsx
+++ src/screens/TripsScreen/TripsScreenComponent.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import LottieView from "lottie-react-native";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
@@ -6,7 +6,9 @@ import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-nat
 const TripsScreenComponent = () => {
   return (
     <View>
-      <Text>TripsScreenComponent</Text>
+      <Text style={{
+        
+      }}>Trips</Text>
       <LottieView
       source={require('./../../../assets/lottie/pondering.json')}
       style={{width: "100%", height: "100%"}}
