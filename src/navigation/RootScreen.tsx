44786b02dd3863ab5e24ad872fa28032fc486e67
Shrikant Jha <shrikantjha11@gmail.com>
2024-03-17 16:29:47 +0530

    bottom navigation added

--- src/navigation/RootScreen.tsx
+++ src/navigation/RootScreen.tsx
@@ -3,16 +3,16 @@ import React from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { PaperProvider } from 'react-native-paper'
 import { NavigationContainer } from '@react-navigation/native';
+import MainScreen from './ScreensStack';
+// import ProfileScreenComponent from '../screens/ProfileScreen/ProfileScreenComponent';
 
 const RootScreen = () => {
     return (
     <NavigationContainer>
-
         <PaperProvider>
             <SafeAreaProvider>
-                <View>
-                    <Text>RootScreen</Text>
-                </View>
+                    {/* <ProfileScreenComponent /> */}
+                    <MainScreen />
             </SafeAreaProvider>
         </PaperProvider>
         </NavigationContainer>
